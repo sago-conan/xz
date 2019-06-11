1278761737da2f6021b6c76bd9037148ed38098e
suwei.air <suwei.air@gmail.com>
2019-06-12 00:20:37 +0800

    添加Windows平台编译

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import os
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
+from conans import AutoToolsBuildEnvironment, ConanFile, MSBuild, tools
 
 
 class XzConan(ConanFile):
@@ -28,26 +28,68 @@ class XzConan(ConanFile):
     def _xz_folder(self):
         return os.path.join(self.source_folder, self._xz_folder_name)
 
+    @property
+    def _xz_vs_folder(self):
+        compiler_version = int(self.settings.compiler.version.value)
+        vs_folder = "vs2017" if compiler_version >= 15 else "vs2013"
+        return os.path.join(self._xz_folder, "windows", vs_folder)
+
+    @property
+    def _vs_arch_map(self):
+        return {"x86": "Win32", "x86_64": "x64"}
+
+    @property
+    def _vs_target(self):
+        return "liblzma_dll" if self.options.shared else "liblzma"
+
+    @property
+    def _vs_build_type(self):
+        # return "Debug" if self.settings.build_type == "Debug" else "Release"
+        return "Release"
+
     def source(self):
         url = "https://tukaani.org/xz/{}.tar.xz".format(self._xz_folder_name)
         tools.get(url)
 
     def build(self):
-        with tools.chdir(self._xz_folder):
-            build = AutoToolsBuildEnvironment(self)
-            args = [
-                "--disable-xz", "--disable-xzdec", "--disable-lzmadec",
-                "--disable-lzmainfo", "--disable-lzma-links",
-                "--disable-scripts", "--disable-doc"
-            ]
-            if "fPIC" in self.options and self.options.fPIC:
-                args.append("--with-pic")
+        if self.settings.compiler == "Visual Studio":
+            build = MSBuild(self)
+            build.build(
+                os.path.join(self._xz_vs_folder, "xz_win.sln"),
+                targets=[self._vs_target],
+                upgrade_project=False,
+                build_type=self._vs_build_type,
+                platforms=self._vs_arch_map)
+        else:
+            with tools.chdir(self._xz_folder):
+                build = AutoToolsBuildEnvironment(self)
+                args = [
+                    "--disable-xz", "--disable-xzdec", "--disable-lzmadec",
+                    "--disable-lzmainfo", "--disable-lzma-links",
+                    "--disable-scripts", "--disable-doc"
+                ]
+                if "fPIC" in self.options and self.options.fPIC:
+                    args.append("--with-pic")
+                if self.options.shared:
+                    args.extend(["--disable-static", "--enable-shared"])
+                else:
+                    args.extend(["--enable-static", "--disable-shared"])
+                # if self.settings.build_type == "Debug":
+                #     args.append("--enable-debug")
+                build.configure(args=args)
+                build.make()
+                build.install()
+
+    def package(self):
+        if self.settings.compiler == "Visual Studio":
+            header_dir = os.path.join(self._xz_folder, "src", "liblzma", "api")
+            self.copy(
+                pattern="*.h", dst="include", src=header_dir, keep_path=True)
+            arch = self._vs_arch_map.get(str(self.settings.arch))
+            bin_dir = os.path.join(self._xz_vs_folder, str(
+                self._vs_build_type), arch, self._vs_target)
+            self.copy(pattern="*.lib", dst="lib", src=bin_dir, keep_path=False)
+            self.copy(pattern="*.pdb", dst="bin", src=bin_dir, keep_path=False)
             if self.options.shared:
-                args.extend(["--disable-static", "--enable-shared"])
-            else:
-                args.extend(["--enable-static", "--disable-shared"])
-            # if self.settings.build_type == "Debug":
-            #     args.append("--enable-debug")
-            build.configure(args=args)
-            build.make()
-            build.install()
+                self.copy(
+                    pattern="*.dll", dst="bin", src=bin_dir, keep_path=False)
